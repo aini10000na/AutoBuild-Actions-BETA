17944a4ab28edf922ac340f04f4f5c083b192d78
Code008 <aini10000na@gmail.com>
2025-12-24 11:05:17 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -149,6 +149,11 @@ EOF
 				AddPackage passwall xiaorouji openwrt-passwall main
 			    AddPackage passwall xiaorouji openwrt-passwall-packages main
 				AddPackage passwall xiaorouji openwrt-passwall2 main
+				# 根据https://github.com/xiaorouji/openwrt-passwall2/issues/722#issuecomment-2560962548
+				# 添加此命令解决passwall冲突
+				rm -rf ${WORK}/feeds/packages/net/{chinadns*,hysteria,geoview,trojan*,xray*,v2ray*,sing*}
+    			rm -rf ${FEEDS_PKG}/net/{chinadns*,hysteria,geoview,trojan*,xray*,v2ray*,sing*}
+				rm -rf ${WORK}/feeds/luci/applications/luci-app-passwall/
 				rm -r ${FEEDS_LUCI}/luci-app-passwall
 				#AddPackage other WROIATE luci-app-socat main
     			#rm -r ${FEEDS_LUCI}/luci-app-socat
